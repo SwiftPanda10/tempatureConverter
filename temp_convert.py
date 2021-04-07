60f94ef6116a56f7bf92acafce5ad9393caf18ea
Sam Bennett <samuelhb201@gmail.com>
2021-04-07 11:18:49 -0700

    Prints the equivalent temperature in Fahrenheit from Celsius

---
+++ temp_convert.py
@@ -0,0 +1,10 @@
+# Author: Samuel Bennett
+# Date: 4/7/2021
+# Description: Prints the equivalent temperature in Fahrenheit from Celsius
+
+print("This program gives the temperature in Fahrenheit from Celsius")
+temp_c = float(input("Please enter temperature in Celsius: "))
+
+temp_f = temp_c * (9/5) + 32
+
+print("The equivalent temperature in Fahrenheit is: ", temp_f)
